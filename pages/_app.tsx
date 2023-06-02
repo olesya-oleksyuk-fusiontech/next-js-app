2674c8da7fc025f30169d7cf97d74f8e8d4b3ee5
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-06-02 17:56:27 +0300

    add dark/light mode

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
 import '../styles/global.css';
 import { AppProps } from 'next/app';
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyles } from '../styles/Global';
+import { darkTheme, lightTheme } from '../styles/Theme';
+import { Toggle } from '../components/toggler';
 
 export default function App({ Component, pageProps } : AppProps) {
-  return <Component {...pageProps} />;
+  const [theme, setTheme] = useState('light');
+  const toggleTheme = () => {
+    // eslint-disable-next-line no-unused-expressions
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
+
+  return (
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <Toggle theme={theme} toggleTheme={toggleTheme} />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }

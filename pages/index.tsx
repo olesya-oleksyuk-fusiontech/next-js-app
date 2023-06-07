93c36453c9265b5da28238f5b0a113a44dc18a97
Olesya Oleksyuk <olesya.oleksyuk@fusion-tech.pro>
2023-06-07 12:59:38 +0300

    leave styled components in index (home) page until I find a solution

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,30 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import React from 'react';
+import styled from 'styled-components';
 import { getSortedPostsData } from '../lib/posts';
 import Heading from '../components/Heading';
 import DateNote from '../components/DateNote';
 import Layout, { siteTitle } from '../containers/Layout';
-import { AboutMe, Blogs } from './index.styles';
+
+export const AboutMe = styled.section`
+  font-size: ${({ theme }) => theme.font.size.md};
+  line-height: 1.5;
+`;
+
+export const Blogs = styled(AboutMe)`
+  padding-top: 1px;
+
+  .blog__list {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  .blog__item {
+    margin: 0 0 1.25rem;
+  }
+`;
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
